288271a13341135ea6e45c99f0de150c8d4eda17
yaniv kalfa <netameta1@yahoo.com>
2016-07-08 22:52:36 -0700

    change interceptors and version

--- src/lib/http.js
+++ src/lib/http.js
@@ -6,22 +6,17 @@ let options = {
   defaults:{}
 };
 
+let initInterceptors = {
+  request: null,
+  response: null
+};
+
 function HTTP(ignoreOptions){
   return HTTP.getInstance(ignoreOptions);
 }
 
 HTTP.setOptions = function (extendedDefaults) {
-  options = Object.assign(options, extendedDefaults);
-};
-
-HTTP.getOptions = function() {
-  return options;
-};
-
-HTTP.getInstance = function(ignoreOptions) {
-  
-  if (ignoreOptions) return axios;
-  
+  Object.assign(options, extendedDefaults);
   Object.assign(axios.defaults, options.defaults);
 
   if (options.interceptors) {
@@ -31,8 +26,13 @@ HTTP.getInstance = function(ignoreOptions) {
       request.push(interceptors.requestSuccess || pass);
       request.push(interceptors.requestError || pass);
 
+      if ( initInterceptors.request ) {
+        axios.interceptors.request.eject(initInterceptors.request);
+        initInterceptors.request = null;
+      }
+
       if ( request.length ) {
-        axios.interceptors.request.use.apply(axios.interceptors.request, request);
+        initInterceptors.request = axios.interceptors.request.use.apply(axios.interceptors.request, request);
       }
     }
 
@@ -41,12 +41,23 @@ HTTP.getInstance = function(ignoreOptions) {
       response.push(interceptors.responseSuccess || pass);
       response.push(interceptors.responseError || pass);
 
+      if ( initInterceptors.response ) {
+        axios.interceptors.response.eject(initInterceptors.response);
+        initInterceptors.response = null;
+      }
+
       if ( response.length ) {
-        axios.interceptors.response.use.apply(axios.interceptors.response, response);
+        initInterceptors.response = axios.interceptors.response.use.apply(axios.interceptors.response, response);
       }
     }
   }
+};
 
+HTTP.getOptions = function() {
+  return options;
+};
+
+HTTP.getInstance = function(ignoreOptions) {
   return axios;
 };
 
